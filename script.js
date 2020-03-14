4878423156a18e0030a2c0c5bf77d68749764511
daricat <darina.talk@gmail.com>
2020-03-14 23:20:46 +0300

    feat: add slider switch and activate phone screen

--- script.js
+++ script.js
@@ -6,6 +6,105 @@ menu.addEventListener("click", (event) => {
     event.target.classList.add('header__item-active');
 });
 
+
+// Slider. Slides switching
+
+const slider = document.getElementsByClassName('slider__background')[0];
+
+const section_slider = document.getElementsByClassName('slider')[0];
+
+const next = document.getElementsByClassName('fa-chevron-right')[0];
+const prev = document.getElementsByClassName('fa-chevron-left')[0];
+
+let slides = slider.getElementsByClassName('slide');
+
+let count = 0;
+
+slides[0].classList.add('active_slide');
+section_slider.classList.add('slide1_bg');
+
+function next_slide() {
+    count += 1;    
+    
+    if ( count == slides.length ) {        
+        slides[count-1].classList.remove('active_slide');        
+        count = 0;                
+        slides[count].classList.add('active_slide');
+        section_slider.classList.remove('slide2_bg');
+        section_slider.classList.add('slide1_bg');
+    } else {
+        slides[count-1].classList.remove('active_slide');
+        slides[count].classList.add('active_slide');
+        section_slider.classList.remove('slide1_bg');
+        section_slider.classList.add('slide2_bg');
+    }    
+}
+
+function prev_slide() {
+    count -= 1;
+    
+    if ( count < 0 ) {
+        slides[0].classList.remove('active_slide');
+        count = slides.length - 1;
+        slides[count].classList.add('active_slide');
+        section_slider.classList.remove('slide1_bg');
+        section_slider.classList.add('slide2_bg');
+    } else {
+        slides[count+1].classList.remove('active_slide');
+        slides[count].classList.add('active_slide');
+        section_slider.classList.remove('slide2_bg');
+        section_slider.classList.add('slide1_bg');
+    }
+}
+
+next.addEventListener('click', next_slide);
+prev.addEventListener('click', prev_slide);
+
+
+// Phone Activating Phone Screens
+
+const phone_button_vertical = document.getElementsByClassName('bg_button-vertical')[0];
+const phone_button_horizontal = document.getElementsByClassName('bg_button-horizontal')[0];
+const phone_button_slide2 = document.getElementsByClassName('bg_button_slide2')[0];
+
+const bg_black_vertical = document.getElementsByClassName('bg_black-vertical')[0];
+const bg_black_horizontal = document.getElementsByClassName('bg_black-horizontal')[0];
+const bg_black_slide2 = document.getElementsByClassName('bg_black_slide2')[0];
+
+function on_off_vertical() {
+    let style_vertical = getComputedStyle(document.getElementsByClassName('bg_black-vertical')[0]);    
+    
+    if ( style_vertical.display == 'none' ) {
+        bg_black_vertical.classList.add('bg_black-active');
+    } else {
+        bg_black_vertical.classList.remove('bg_black-active');
+    }
+}
+
+function on_off_horizontal() {
+    let style_horizontal = getComputedStyle(document.getElementsByClassName('bg_black-horizontal')[0]);
+
+    if ( style_horizontal.display == 'none' ) {
+        bg_black_horizontal.classList.add('bg_black-active');
+    } else {
+        bg_black_horizontal.classList.remove('bg_black-active');
+    }
+}
+
+function on_off_slide2() {
+    let style_slide2 = getComputedStyle(document.getElementsByClassName('bg_black_slide2')[0]);
+
+    if ( style_slide2.display == 'none' ) {
+        bg_black_slide2.classList.add('bg_black-active');
+    } else {
+        bg_black_slide2.classList.remove('bg_black-active');
+    }
+}
+
+phone_button_vertical.addEventListener('click', on_off_vertical);
+phone_button_horizontal.addEventListener('click', on_off_horizontal);
+phone_button_slide2.addEventListener('click', on_off_slide2);
+
 // Portfolio Tab Switching
 const portfolio_gallery = document.getElementById('portfolio__gallery');
 const portfolio_nav = document.getElementsByClassName('portfolio__navigation')[0];
